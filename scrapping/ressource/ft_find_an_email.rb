4ff0328bd7809e751327ceea0bbf4af69f71ffe5
ferdivdb <ferdivdb@hotmail.com>
2018-10-11 14:40:57 +0200

    add finder

---
+++ scrapping/ressource/ft_find_an_email.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'nokogiri'
+
+def mail_finder(url, tbody)
+    doc = Nokogiri::HTML(open(url))
+    doc.xpath(tbody).each do |node|
+    	node.text.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i) { |node| puts node } 
+  	end
+ end
+#mail_finder(#,#)
+##exemple
+#mail_finder("http://annuaire-des-mairies.com/95/vaureal.html",'//tbody[1]/tr[4]/td[2]')
